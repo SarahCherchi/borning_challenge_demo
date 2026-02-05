4190c686b599995d1435a028a71a075551a207de
SarahCherchi <csarah@live.be>
2026-02-05 20:35:55 +0100

    Front : Service activite  getUserActivities  getTeamActivities

---
+++ frontend/flutter_application_1/lib/services/activite_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/activite.dart';
+
+class ApiService {
+  
+  final String baseUrl = "http://127.0.0.1:8000";
+
+  // Récupérer les activités d'un utilisateur
+  Future<List<Activite>> getUserActivities(int idUtilisateur) async {
+    final response = await http.get(Uri.parse('$baseUrl/utilisateur/$idUtilisateur/activites'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Activite.fromJson(data)).toList();
+    } else {
+      throw Exception('Erreur lors du chargement des activités');
+    }
+  }
+
+  // Récupérer les activités d'une équipe
+  Future<List<Activite>> getTeamActivities(int idEquipe) async {
+    final response = await http.get(Uri.parse('$baseUrl/equipe/$idEquipe/activites'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Activite.fromJson(data)).toList();
+    } else {
+      throw Exception('Erreur lors du chargement des activités de l\'équipe');
+    }
+  }
+}
